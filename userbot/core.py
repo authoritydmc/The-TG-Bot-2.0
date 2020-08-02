0c0b214de6f9db99dcdf199777a642f734313b41
authoritydmc <com.mailuser@gmail.com>
2020-08-02 16:08:27 +0530

    updated alive'

--- userbot/core.py
+++ userbot/core.py
@@ -148,7 +148,7 @@ async def alive(event):
     username = f"\nUser: `{user}\n"
     memory = psutil.virtual_memory()
     specs = f"`System: {uname.system}\nRelease: {uname.release}\nVersion: {uname.version}\nProcessor: {uname.processor}\nMemory [RAM]: {get_size(memory.total)}`"
-    help_string = f"**// BEASTBOT v3 is running //**\n\n**General Info:**\n`Build Version: {build} {username}`Github Repository: `{Config.GITHUB_REPO_LINK}\n\n**System Specifications:**\n{specs}\n```Python: {sys.version}```\n```Telethon: {__version__}```\n\n**Contact developer:** [justaprudev](https://t.me/justaprudev) \n**Update channel:** [Join](https://t.me/The_TG_Bot) \n**Support group:** [Join](https://t.me/The_TG_Bot_Support)"
+    help_string = f"**// BEASTBOT v3 is running //**\n\n**General Info:**\n`Build Version: {build} {username}`Github Repository: `{Config.GITHUB_REPO_LINK}\n\n**System Specifications:**\n{specs}\n```Python: {sys.version}```\n```Telethon: {__version__}```\n\n**Contact developer:** [authoritydmc](https://t.me/beast0110) \n**Update channel:** [Join](https://t.me/beastbotofficial) \n"
     await client.send_file(
         event.chat_id,
         caption=help_string,
